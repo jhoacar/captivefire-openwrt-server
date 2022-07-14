0e6649c800398dd841b12d16d4a2cf140326d689
jhoacar <carrerojhoan@gmail.com>
2022-07-14 17:15:11 -0300

    added phpdox documentation

--- src/GraphQL/Context.php
+++ src/GraphQL/Context.php
@@ -1,19 +1,36 @@
 <?php
 
+/*
+ * This file is part of the Captivefire package.
+ *
+ * (c) Jhoan Carrero <jhoacar@captivefire.net>
+ *
+ */
+
 namespace App\GraphQL;
 
 use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class used by send all the information inside each resolver for GraphQL.
+ * 
+ * @author Jhoan Carrero <jhoacar@captivefire.net>
+ * 
+ * @api
  */
 class Context
 {
     /**
-     * @var Request
+     * @var \Symfony\Component\HttpFoundation\Request
      */
     public $request;
 
+    /**
+     * Constructor.
+     * @param void
+     * 
+     * @api
+     */
     public function __construct()
     {
     }
