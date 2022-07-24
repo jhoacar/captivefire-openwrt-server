c9b304e189e748f58459258cc4ab926c0e5cbd21
jhoacar <carrerojhoan@gmail.com>
2022-07-24 12:28:27 -0300

    added php.ini configuration for humbug/box

--- config/app.php
+++ config/app.php
@@ -6,4 +6,7 @@ return [
     'graphql' => [
         'uri' => $_ENV['APP_GRAPHQL_ROUTE'] ?? '/graphql',
     ],
+    'curl' => [
+        'uri' => $_ENV['CAPTIVEFIRE_ACCESS'] ?? 'locahost:4000/openwrt/graphql'
+    ]
 ];
