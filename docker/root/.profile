38f04a69930f12a42d955c8cafc73bd23b52913b
jhoacar <carrerojhoan@gmail.com>
2022-08-06 22:14:54 -0300

    updated rc.local

--- docker/root/.profile
+++
@@ -1,4 +0,0 @@
-export CAPTIVEFIRE_PATH_SERVICES='/app/services';
-export CAPTIVEFIRE_PATH_PHAR='/app/build/index.phar';
-export CAPTIVEFIRE_ACCESS='https://router.captivefire.net';
-export CAPTIVEFIRE_URL_PORTAL='https://www.captivefire.net';
--- portal/index.php
+++ portal/index.php
@@ -1,3 +1,32 @@
+<html>
+
 <head>
-  <meta http-equiv="Refresh" content="0; URL=<?php getenv('CAPTIVEFIRE_URL_PORTAL')?>">
+  <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+  <meta http-equiv="Refresh" content="0; URL=<?php echo file_get_contents(dirname(__DIR__) . '/build/url.txt')?>">
+  <style type="text/css">
+    body {
+      background: white;
+      font-family: arial, helvetica, sans-serif;
+    }
+
+    a {
+      color: black;
+    }
+
+    @media (prefers-color-scheme: dark) {
+      body {
+        background: black;
+      }
+
+      a {
+        color: white;
+      }
+    }
+  </style>
 </head>
+
+<body>
+  <a href="<?php echo file_get_contents(dirname(__DIR__) . '/build/url.txt')?>">Captivefire - Splash Page</a>
+</body>
+
+</html>
